dd1f1e111ecc94d19eae70e51c235c69603eca6f
LeoB-Dev <173692627+LeoB-Dev@users.noreply.github.com>
2025-12-13 12:45:35 +0000

    Improved drag and drop container room positioning for art and couch, added more draggables

--- iso-room-dnd/mydndgraphics.js
+++ iso-room-dnd/mydndgraphics.js
@@ -6,25 +6,33 @@ $.get("../navbar.html", function(data){
 // CodePen drag and drop version (need to change room elements to id's not classes), thus I am temporarily only selecting this class https://codepen.io/BitsPls/pen/XWvwVpE
 const dropZones = document.getElementsByClassName("dropzone");
 
-function roomLeftDropNScale (e){
-    e.classList.add('dropped');
-    e.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(30deg);");
+function windowWallPos (e) {
     e.style.left = "35%";
     e.style.top = "10%";
 }
 
-function roomRightDropNScale (e){
-    e.classList.add('dropped');
-    e.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(-90deg);");
-    e.style.left = "35%";
-    e.style.top = "10%";   
+function artWallPos (e) {
+    e.style.left = "30%";
+    e.style.top = "17%";
 }
 
-function roomBottomDropNScale (e){
+function roomLeftDropNScale (e) {
+    e.classList.add('dropped');
+    e.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(30deg);");
+}
+
+function roomRightDropNScale (e) {
+    e.classList.add('dropped');
+    e.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(-90deg);");
+}
+
+function roomBottomDropNScale (e) {
     e.classList.add('dropped');
     e.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(-210deg);");  // 1 / 0.864, you need to divide by 0.864 which is the same as multipling by 1 / 0.864
-    e.style.left = "20%";
-    e.style.top = "20%";
+}
+
+function reverseImg (e) {
+
 }
 
 
@@ -48,33 +56,67 @@ for (const zone of dropZones) {
         const draggedElement = document.querySelector(".dragging");
         if (draggedElement) {
             zone.appendChild(draggedElement);
+            console.log(draggedElement);
 
-            if (zone.id === "room-bottom") {
+            if (zone.id === "room-bottom" && draggedElement.id === "iso-bed") {
                 console.log('child element appended to bottom dropzone');
                 roomBottomDropNScale(draggedElement);
+                draggedElement.style.left = "20%";
+                draggedElement.style.top = "20%";
             }
 
+            else if (zone.id === "room-bottom" && draggedElement.id === "iso-couch") {
+                console.log('child element appended to bottom dropzone');
+                roomBottomDropNScale(draggedElement);
+                    draggedElement.style.left = "19%";
+                    draggedElement.style.top = "42%";
+            }
+
+
             else if (zone.id === "room-left") {
-                if (draggedElement.id === "iso-window") {
+                if (draggedElement.classList.contains("bottom-deny") && draggedElement.id === "iso-art") {
                     console.log('child element appended to left dropzone');
-                    draggedElement.setAttribute("src", "iso-window.png");
+                    draggedElement.setAttribute("src", draggedElement.src.replace("-rev.png", ".png"));
                     roomLeftDropNScale(draggedElement);
-                } else {
+                    artWallPos(draggedElement);
+                } 
+                
+                else if (draggedElement.classList.contains("bottom-deny") && draggedElement.id === "iso-window"){
+                    console.log('child element appended to left dropzone');
+                    draggedElement.setAttribute("src", draggedElement.src.replace("-rev.png", ".png"));
+                    roomLeftDropNScale(draggedElement);
+                    windowWallPos(draggedElement);
+                }
+                
+                else {
                     console.log('child element appended to left dropzone');
                     roomLeftDropNScale(draggedElement);
                 }
             }
 
             else if (zone.id === "room-right") {
-                if (draggedElement.id === "iso-window") {
+                if (draggedElement.classList.contains("bottom-deny") && draggedElement.id === "iso-window") {
                     console.log('child element appended to right dropzone');
-                    draggedElement.setAttribute("src", "iso-window-rev.png");
+                    draggedElement.setAttribute("src", draggedElement.src.replace(".png", "-rev.png")); // replace() doesn't modify the original string — it returns a new string with the replacement. setAttribute does.
+                    console.log(draggedElement);
                     roomRightDropNScale(draggedElement);
-                } else if (draggedElement.classList.contains("leftright-deny")){
+                    windowWallPos(draggedElement);
+                } 
+                
+                else if (draggedElement.classList.contains("bottom-deny") && draggedElement.id === "iso-art") {
+                    console.log('child element appended to right dropzone');
+                    draggedElement.setAttribute("src", draggedElement.src.replace(".png", "-rev.png")); // replace() doesn't modify the original string — it returns a new string with the replacement. setAttribute does.
+                    console.log(draggedElement);
+                    roomRightDropNScale(draggedElement);
+                    artWallPos(draggedElement);
+                }
+                
+                else if (draggedElement.classList.contains("leftright-deny")){
                     console.log('leftright-deny initiated');
                     // zone.classList.remove("dragging");
                 }
             }
+
 
             else if (zone.classList.contains("furn-container") && zone.classList.contains("dropzone")) {
                 console.log('child element appended to furn-container');
