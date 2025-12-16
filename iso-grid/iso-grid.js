c3eaf838617b5b4a7926ac3b5d08fa446357cbe0
Leo B Dev <173692627+LeoB-Dev@users.noreply.github.com>
2025-12-16 10:33:40 +0000

    Streamlined iso-grid javascript

--- iso-grid/iso-grid.js
+++ iso-grid/iso-grid.js
@@ -1,12 +1,10 @@
-let x = 0;
-while (x < 10) {
+for (let i=0; i < 10; i++) {
     const newSquare = document.createElement("div");
     newSquare.classList.add('row');
     const gridContainer = document.getElementById("grid-container");
     gridContainer.appendChild(newSquare);
-    x++;
-    let y = 0;
 }
+
 
 for (let i=0; i < 10; i++) {
     const rowElement = document.getElementsByClassName("row");
